d65470d5579edba0520546b824d867f31695f387
imarcowenzel <marcowenzelcirilo@gmail.com>
2025-02-07 09:48:48 -0300

    Changing folder structure and adding error handling

--- client-app/src/app/(admin)/marco/page.tsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>marco</div>;
-}
-
-export default Home;
---
+++ client-app/src/app/(root)/page.tsx
@@ -0,0 +1,30 @@
+import NavBar from "@/components/NavBar";
+import { Activity } from "@/types";
+
+async function Home() {
+  try {
+    const response = await fetch("http://localhost:5000/api/activities");
+
+    if (!response.ok) {
+      throw new Error(`Erro HTTP: ${response.status}`);
+    }
+
+    const activities: Activity[] = await response.json();
+
+    return (
+      <div>
+        <NavBar />
+        <ul>
+          {activities.map((activity) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    console.error("Erro ao buscar atividades:", error);
+    return <div>Erro ao carregar atividades. Verifique o console.</div>;
+  }
+}
+
+export default Home;

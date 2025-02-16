d270b8337426768a3d767e91836a8b8c1a55f21a
imarcowenzel <marcowenzelcirilo@gmail.com>
2025-02-16 15:17:54 -0300

    Adding API error messages

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Application.Core;
+using AutoMapper;
 using Domain;
 using MediatR;
 using Persistence;
@@ -8,31 +9,38 @@ namespace Application.Activities
     public class Edit
     {
 
-        public class Command: IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
 
-            public Handler(DataContext context, IMapper mapper) {
+            public Handler(DataContext context, IMapper mapper)
+            {
                 _context = context;
                 _mapper = mapper;
             }
 
             public IMapper Mapper { get; }
 
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
 
+                if (activity == null) return null;
+
                 _mapper.Map(request.Activity, activity);
 
-                await _context.SaveChangesAsync();
-        
+                var result = await _context.SaveChangesAsync() > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to update activity");
+
+                return Result<Unit>.Success(Unit.Value);
+
             }
         }
 
--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,5 @@
-﻿using Domain;
+﻿using Application.Core;
+using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -8,11 +9,8 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<Activity>>
-        {
-
-        }
-        public class Handler : IRequestHandler<Query, List<Activity>>
+        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
         {
             private readonly DataContext _context;
 
@@ -23,10 +21,10 @@ namespace Application.Activities
 
             public ILogger<List> Logger { get; }
 
-            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
             {
 
-                return await _context.Activities.ToListAsync();
+                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
             }
         }
     }

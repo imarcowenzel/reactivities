501c2b1589ec0a1033f2599482157e73b0ce7980
imarcowenzel <marcowenzelcirilo@gmail.com>
2025-02-17 11:06:10 -0300

    Criando user identity, fazendo seed e criando DTO

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,7 @@
 using API.Extensions;
 using API.Middleware;
-using Application.Activities;
-using Application.Core;
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -32,8 +33,9 @@ var services = scope.ServiceProvider;
 try
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager = services.GetRequiredService<UserManager<AppUser>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedData(context);
+    await Seed.SeedData(context, userManager);
 }
 catch (Exception ex)
 {

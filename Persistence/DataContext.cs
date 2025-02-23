aeb76cb208838af4bef74c72f3b8d9c239188598
imarcowenzel <marcowenzelcirilo@gmail.com>
2025-02-23 16:21:43 -0300

    Finishing following feature

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -9,10 +9,10 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<UserFollowing> UserFollowings { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -29,6 +29,18 @@ namespace Persistence
                 .HasOne(a => a.Activity)
                 .WithMany(u => u.Attendees)
                 .HasForeignKey(aa => aa.ActivityId);
+            builder.Entity<UserFollowing>(b =>
+            {
+                b.HasKey(k => new { k.ObserverId, k.TargetId });
+                b.HasOne(o => o.Observer)
+                    .WithMany(f => f.Followings)
+                    .HasForeignKey(o => o.ObserverId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                b.HasOne(o => o.Target)
+                    .WithMany(f => f.Followers)
+                    .HasForeignKey(o => o.TargetId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
